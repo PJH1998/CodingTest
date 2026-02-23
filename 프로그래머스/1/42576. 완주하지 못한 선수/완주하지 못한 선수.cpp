1998dbc7b44c15075c5768afacb8903303eeecb1
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2026-02-23 16:28:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 176.89 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    map<string, int> check;
+    
+    for(auto& name : participant)
+    {
+        auto iter = check.find(name);
+        if(iter == check.end())
+            check.emplace(name, 0);
+        ++check[name];
+    }
+    
+    for(auto& name : completion)
+        --check[name];
+    
+    for(auto& Pair : check)
+    {
+        if(Pair.second > 0)
+        {
+            answer = Pair.first;
+            break;
+        }
+    }
+    
+    return answer;
+}
